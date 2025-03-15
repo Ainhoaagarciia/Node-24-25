1427c58d2ee48a63de0cccdfa5721600c18d4dc4
Ainhoaagarciia <ainhoagarciasanz02@gmail.com>
2025-03-16 00:17:16 +0100

    Conexion BD Front y Back

---
+++ 008-Pruebas Conexion BD Back y Front/04-Prueba-4 (Filtrar Por Titulo y Ordenar Año)/Prueba-4/Back/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const {Pool} = require('pg');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'prueba-2.cknylvg9zp2l.us-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    password: '12345678',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/peliculas', async (req, res) => {
+    const {rows} = await pool.query(`SELECT p.id_pelicula, p.titulo_pelicula, p.director_pelicula, p.anio_pelicula, p.portada_pelicula, g.nombre_genero
+                                    FROM peliculas p
+                                    JOIN generos g ON p.id_genero = g.id_genero`);
+    res.json(rows);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
---
+++ 008-Pruebas Conexion BD Back y Front/04-Prueba-4 (Filtrar Por Titulo y Ordenar Año)/Prueba-4/Front/main.js
@@ -0,0 +1,80 @@
+const listadoPeliculas = document.getElementById('peliculas');
+const generoSelect = document.getElementById("genero");
+const buscarInput = document.getElementById("buscar");
+const buscarBtn = document.getElementById("buscar-button");
+const ordenarAscBtn = document.getElementById("ordenar-ascendente");
+const ordenarDescBtn = document.getElementById("ordenar-descendente");
+
+let peliculas = [];
+let peliculasFiltradas = [];
+function inicializar(){
+    fetch('http://localhost:3000/peliculas')
+    .then(response => response.json())
+    .then(data => {
+        peliculas = data;
+        peliculasFiltradas = [...peliculas];
+        mostrarPeliculas(peliculasFiltradas);
+        cargarGeneros();
+    })
+    .catch(error => { console.log("Error al cargar las peliculas", error)});
+}
+function mostrarPeliculas(lista){
+    listadoPeliculas.innerHTML = '';
+    if (lista.length === 0) {
+        listadoPeliculas.innerHTML = '<p>No hay peliculas</p>';
+        return;
+    }else{
+        lista.forEach(pelicula => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <div class="peliculas">
+                    <img src="${pelicula.portada_pelicula}" alt="${pelicula.titulo}">
+                    <h2>${pelicula.titulo_pelicula}</h2>
+                    <p>Director: ${pelicula.director_pelicula}</p>
+                    <p>Año: ${pelicula.anio_pelicula}</p>
+                    <p>Género: ${pelicula.nombre_genero}</p>
+                </div>
+                `;
+            listadoPeliculas.appendChild(li);
+        });
+    }
+}
+function cargarGeneros() {
+    generoSelect.innerHTML = '<option value="todos">Todos</option>';
+    const generos = [...new Set(peliculas.map(p => p.nombre_genero))];
+
+    generos.forEach(genero => {
+        const option = document.createElement("option");
+        option.value = genero;
+        option.textContent = genero;
+        generoSelect.appendChild(option);
+    });
+
+    generoSelect.addEventListener("change", () => filtrarPeliculas(generoSelect.value));
+}
+function filtrarPeliculas(genero) {
+    if (genero === "todos") {
+        peliculasFiltradas = [...peliculas];
+    } else {
+        peliculasFiltradas = peliculas.filter(pelicula => pelicula.nombre_genero === genero);
+    }
+    mostrarPeliculas(peliculasFiltradas);
+}
+
+function buscarPorTitulo() {
+    const query = buscarInput.value.toLowerCase();
+    const peliculasEncontradas = peliculas.filter(pelicula =>
+        pelicula.titulo_pelicula.toLowerCase().includes(query)
+    );
+    mostrarPeliculas(peliculasEncontradas);
+}
+// Ordenar películas por año
+function ordenarPeliculas(ascendente = true) {
+    peliculasFiltradas.sort((a, b) => (ascendente ? a.anio_pelicula - b.anio_pelicula : b.anio_pelicula - a.anio_pelicula));
+    mostrarPeliculas(peliculasFiltradas);
+}
+
+buscarBtn.addEventListener("click", buscarPorTitulo);
+ordenarAscBtn.addEventListener("click", () => ordenarPeliculas(true));
+ordenarDescBtn.addEventListener("click", () => ordenarPeliculas(false));
+inicializar();
